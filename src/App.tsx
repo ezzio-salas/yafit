88a3d1e29bf78c2cacfb06af379858b885f50f17
ezzio salas <ezzio.salas@gmail.com>
2022-09-13 20:52:05 -0500

    build(sentry):sentry setup

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <button onClick={methodDoesNotExist}>Break the world</button>;
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+Sentry.init({
+  dsn: "https://a2ae84afee6148159f08f2ac4dbdbbcb@o1409338.ingest.sentry.io/6745523",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 root.render(
   <React.StrictMode>
     <App />
